6c538c1289e50cbf360c137c51c1d1afbceb944d
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-03 18:27:13 -0400

    created login.ejs. Made a route to the page and added partials

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -12,5 +12,11 @@ router.get('/' , (req, res) => {
   res.render('index.ejs');
 });
 
+//===========
+// LOGIN
+//===========
+router.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
 
 module.exports = router;
