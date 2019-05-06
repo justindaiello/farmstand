96945bb0e0b7415ee788ae550f4e070831a3d7c7
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-06 16:15:13 -0400

    Added mobile responsiveness to the create form on new.ejs. The create product button was still in a row with the last item and the whole form was margin left. The form in a now a full column when below 600width and is centered.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,12 +4,13 @@ $(() => {
 
 // Click Listener - when you click the div it expands the p element to make it taller and exposes 'hidden' text below it that was once opaque
 $('.buyer-info').on('click', () => {
+  //disable click function when on a mobile device
   if ($(window).width > 600) {
     $('.buyer-info').toggleClass('show-desc');
   }
 })
 
-//Bootstram modal function
+//Bootstrap modal function
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
