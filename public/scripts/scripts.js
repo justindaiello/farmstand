0d471a2e35a2a24dc6f56a40058be81ada624cb4
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-06 13:51:28 -0400

    Worked on mobile responsiveness. Main text div in index page got resized for mobile & the click to reveal more text function for disabiled

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,7 +4,9 @@ $(() => {
 
 // Click Listener - when you click the div it expands the p element to make it taller and exposes 'hidden' text below it that was once opaque
 $('.buyer-info').on('click', () => {
-  $('.buyer-info').toggleClass('show-desc')
+  if ($(window).width > 600) {
+    $('.buyer-info').toggleClass('show-desc');
+  }
 })
 
 //Bootstram modal function
