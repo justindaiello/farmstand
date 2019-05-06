2d8731e7f84e0f8c5ce2f12c64261be81514eae2
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-06 17:57:16 -0400

    Fixed jQuery listener that was controlling the toggleclass on index.ejs. Listener was causing the class not to toggle at any width. Changed window.width to window.outerWidth in scripts.js and now the class toggles at widths above 600

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,8 +5,10 @@ $(() => {
 // Click Listener - when you click the div it expands the p element to make it taller and exposes 'hidden' text below it that was once opaque
 $('.buyer-info').on('click', () => {
   //disable click function when on a mobile device
-  if ($(window).width > 600) {
+  if (window.outerWidth >= 600) {
     $('.buyer-info').toggleClass('show-desc');
+  } else {
+    return;
   }
 })
 
