1a464c3e7d72b0cc7e41facc07aaf0e45eb54cce
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-08 12:15:51 -0400

    Fixed footer to make it responsive. Moved horizontal line into footer rather than outside.

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -203,105 +203,105 @@ router.delete('/shop/:id/edit', (req, res, next) => {
 //============
 // SEED DATA
 //============
-router.get('/seed', (req, res, next) => {
-  Product.create([
-    {
-      name: 'Sorrel',
-      description: 'Need to step up your plating game? Our red veined sorrel will take you to amateur to pro in an instant.',
-      img: 'https://i.imgur.com/T7DzRyF.jpg',
-      farmName: 'Ellie\'s Farm',
-      price: 14,
-      qty: 10
-    },
-    {
-      name: 'Blueberries',
-      description: 'Pick you own blueberries is sooo last year. We picked them for you and they\'re delicious! Get them before they\'re gone',
-      img: 'https://i.imgur.com/cC6Jz4I.jpg',
-      farmName: 'Archer\'s Farm',
-      price: 4,
-      qty: 30
-    },
-    {
-      name: 'Carrots',
-      description: 'Freshly picked carrots with greens. Use the fresh greens to make carrot-top pesto!',
-      img: 'https://i.imgur.com/wQCvaQ0.jpg',
-      farmName: 'Skylight Farm',
-      price: 4,
-      qty: 10
-    },
-    {
-      name: 'Lettuce',
-      description: 'Fresh heads of red and green butter lettuce. Our lettuce will stay fresh for up to 10 days!',
-      img: 'https://i.imgur.com/c4hAQqW.jpg',
-      farmName: 'Skylight Farm',
-      price: 5,
-      qty: 100
-    },
-    {
-      name: 'Beans',
-      description: 'Freshly picked green and purple beans. These beans are stringless and great raw or cooked.',
-      img: 'https://i.imgur.com/0goMThl.jpg',
-      farmName: 'Archer\'s Farm',
-      price: 3,
-      qty: 40
-    },
-    {
-      name: 'Broccoli',
-      description: 'Frost sweeted heirloom broccoli. This broccoli is super sweet and tender.',
-      img: 'https://i.imgur.com/cufPDiF.jpg',
-      farmName: 'Archer\'s Farm',
-      price: 3,
-      qty: 70
-    },
-    {
-      name: 'Pea Shoots',
-      description: 'Sweet and crunchy pea shoots harvested fresh. These are great in smoothies, salads and sandwhiches.',
-      img: 'https://i.imgur.com/2UnTv8W.jpg',
-      farmName: 'Archer\'s Farm',
-      price: 15,
-      qty: 10
-    },
-    {
-      name: 'Romanesco',
-      description: 'Beautiful and delicious. Romanesco cauliflower is sweet and crunchy and cooks quickly. Quantity is limited so reserve yours now!',
-      img: 'https://i.imgur.com/o5oDDIA.jpg',
-      farmName: 'Ellie\'s Farm',
-      price: 8,
-      qty: 30
-    },
-    {
-      name: 'Cauliflower',
-      description: 'Beautiful and delicious. Orange cauliflower is sweet and crunchy and cooks quickly. Quantity is limited so reserve yours now!',
-      img: 'https://i.imgur.com/vvAOjnb.jpg',
-      farmName: 'Ellie\'s Farm',
-      price: 8,
-      qty: 30
-    },
-    {
-      name: 'Baby Kale',
-      description: 'Full grown kale is overrated! Get some of this super sweet and tender baby kale before its gone!',
-      img: 'https://i.imgur.com/vvAOjnb.jpg',
-      farmName: 'Ellie\'s Farm',
-      price: 10,
-      qty: 50
-    },
-    {
-      name: 'Sweet Peppers',
-      description: 'Our specialty sweet pepper mix is the best in town! These are much sweeter than a traditional bell pepper and are a must try.',
-      img: 'https://i.imgur.com/I6GW062.jpg',
-      farmName: 'Skylight Farm',
-      price: 4,
-      qty: 45
-    },
-    {
-      name: 'Cherry Tomatoes',
-      description: 'You\'ve never had a sweet tomato until you\'ve had a sungold! These are a staff favorite.',
-      img: 'https://i.imgur.com/pr304jx.jpg',
-      farmName: 'Skylight Farm',
-      price: 5,
-      qty: 50
-    },
-  ])
-})
+// router.get('/seed', (req, res, next) => {
+//   Product.create([
+//     {
+//       name: 'Sorrel',
+//       description: 'Need to step up your plating game? Our red veined sorrel will take you to amateur to pro in an instant.',
+//       img: 'https://i.imgur.com/T7DzRyF.jpg',
+//       farmName: 'Ellie\'s Farm',
+//       price: 14,
+//       qty: 10
+//     },
+//     {
+//       name: 'Blueberries',
+//       description: 'Pick you own blueberries is sooo last year. We picked them for you and they\'re delicious! Get them before they\'re gone',
+//       img: 'https://i.imgur.com/cC6Jz4I.jpg',
+//       farmName: 'Archer\'s Farm',
+//       price: 4,
+//       qty: 30
+//     },
+//     {
+//       name: 'Carrots',
+//       description: 'Freshly picked carrots with greens. Use the fresh greens to make carrot-top pesto!',
+//       img: 'https://i.imgur.com/wQCvaQ0.jpg',
+//       farmName: 'Skylight Farm',
+//       price: 4,
+//       qty: 10
+//     },
+//     {
+//       name: 'Lettuce',
+//       description: 'Fresh heads of red and green butter lettuce. Our lettuce will stay fresh for up to 10 days!',
+//       img: 'https://i.imgur.com/c4hAQqW.jpg',
+//       farmName: 'Skylight Farm',
+//       price: 5,
+//       qty: 100
+//     },
+//     {
+//       name: 'Beans',
+//       description: 'Freshly picked green and purple beans. These beans are stringless and great raw or cooked.',
+//       img: 'https://i.imgur.com/0goMThl.jpg',
+//       farmName: 'Archer\'s Farm',
+//       price: 3,
+//       qty: 40
+//     },
+//     {
+//       name: 'Broccoli',
+//       description: 'Frost sweeted heirloom broccoli. This broccoli is super sweet and tender.',
+//       img: 'https://i.imgur.com/cufPDiF.jpg',
+//       farmName: 'Archer\'s Farm',
+//       price: 3,
+//       qty: 70
+//     },
+//     {
+//       name: 'Pea Shoots',
+//       description: 'Sweet and crunchy pea shoots harvested fresh. These are great in smoothies, salads and sandwhiches.',
+//       img: 'https://i.imgur.com/2UnTv8W.jpg',
+//       farmName: 'Archer\'s Farm',
+//       price: 15,
+//       qty: 10
+//     },
+//     {
+//       name: 'Romanesco',
+//       description: 'Beautiful and delicious. Romanesco cauliflower is sweet and crunchy and cooks quickly. Quantity is limited so reserve yours now!',
+//       img: 'https://i.imgur.com/o5oDDIA.jpg',
+//       farmName: 'Ellie\'s Farm',
+//       price: 8,
+//       qty: 30
+//     },
+//     {
+//       name: 'Cauliflower',
+//       description: 'Beautiful and delicious. Orange cauliflower is sweet and crunchy and cooks quickly. Quantity is limited so reserve yours now!',
+//       img: 'https://i.imgur.com/vvAOjnb.jpg',
+//       farmName: 'Ellie\'s Farm',
+//       price: 8,
+//       qty: 30
+//     },
+//     {
+//       name: 'Baby Kale',
+//       description: 'Full grown kale is overrated! Get some of this super sweet and tender baby kale before its gone!',
+//       img: 'https://i.imgur.com/vvAOjnb.jpg',
+//       farmName: 'Ellie\'s Farm',
+//       price: 10,
+//       qty: 50
+//     },
+//     {
+//       name: 'Sweet Peppers',
+//       description: 'Our specialty sweet pepper mix is the best in town! These are much sweeter than a traditional bell pepper and are a must try.',
+//       img: 'https://i.imgur.com/I6GW062.jpg',
+//       farmName: 'Skylight Farm',
+//       price: 4,
+//       qty: 45
+//     },
+//     {
+//       name: 'Cherry Tomatoes',
+//       description: 'You\'ve never had a sweet tomato until you\'ve had a sungold! These are a staff favorite.',
+//       img: 'https://i.imgur.com/pr304jx.jpg',
+//       farmName: 'Skylight Farm',
+//       price: 5,
+//       qty: 50
+//     },
+//   ])
+// })
 
 module.exports = router;
